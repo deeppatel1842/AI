7ef4a963515c2a8e011d4ca054399afee1b1979b
Deep1842 <67943812+deeppatel1842@users.noreply.github.com>
2021-02-25 15:37:28 +0530

    Update naive_classification.R

--- naive Bayes classification/naive_classification.R
+++ naive Bayes classification/naive_classification.R
@@ -1,1 +1,29 @@
 
+
+studata<-read.table("2020_bn_nb_data.txt",head=TRUE) #read data
+colnames(studata) #col. names
+
+#label
+table(studata$QP)# print QP table 
+
+#split Train and Test
+set.seed(20) # random no
+id<-sample(2,nrow(studata),prob = c(0.7,0.3),replace = TRUE) # 70% train and test 
+train<-studata[id==1,]#train data
+test<-studata[id==2,]#test data
+
+nrow(train)
+nrow(test)
+
+#naive bayes
+
+library(e1071)
+library(caret)
+library(rminer)
+
+student_nb<-naiveBayes(QP ~ ., data=train)# Applying naviebayes
+student_nb
+
+pred<-studata.predict(student_nb,test) # predict test data
+mmetric(test$QP,pred,c("ACC","PRECISION","TPR","F1")) # accuracy
+
