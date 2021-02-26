e2b063c22e09e9d90cbebf89b0700dc5a3928351
Deep1842 <67943812+deeppatel1842@users.noreply.github.com>
2021-02-26 11:42:57 +0530

    Update CPT.R

--- naive Bayes classification/CPT.R
+++ naive Bayes classification/CPT.R
@@ -1,3 +1,4 @@
+
 library(bnlearn)
 library(dplyr)
 
@@ -14,6 +15,14 @@ bayes_bn31.net<-bn.fit(bayes_bn31,df)
 plot(bayes_bn31)
 bayes_bn31.net
 bayes_bn31
+bn.fit.barchart(bayes_bn31.net$EC100)
+bn.fit.barchart(bayes_bn31.net$EC160)
+bn.fit.barchart(bayes_bn31.net$IT101)
+bn.fit.barchart(bayes_bn31.net$IT161)
+bn.fit.barchart(bayes_bn31.net$MA101)
+bn.fit.barchart(bayes_bn31.net$PH100)
+bn.fit.barchart(bayes_bn31.net$PH160)
+bn.fit.barchart(bayes_bn31.net$HS101)
 
 bayes_bn32<-hc(df,score = "bic")
 bayes_bn32.net<-bn.fit(bayes_bn32,df)
@@ -25,6 +34,9 @@ bayes_bn32
 
 #q3
 course.grades<-read.table("2020_bn_nb_data.txt",head=TRUE)
-N<-nrow(course.grades)
+bn.fit.barchart(bayes_bn31.net$EC100)N<-nrow(course.grades)
 subset(course.grades,EC100=="DD" & IT101=="CC" & MA101=="CD")
+
+
+
 
